490fd8c98b5bb92145ab8c5a57937f70cf23562e
Albreis - Design & Programação <albreisdev@gmail.com>
2021-10-14 20:39:48 -0300

    Adição do Carbon Fields

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '870e10d0143cba0e364cf00aa2204a8a3a808e3f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '870e10d0143cba0e364cf00aa2204a8a3a808e3f',
+    ),
+    'albreis/carbon-fields' => 
+    array (
+      'pretty_version' => 'v3.3.2',
+      'version' => '3.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd5663e14c6db365323b688dbae1cfbeaf14bee7',
+    ),
+  ),
+);
