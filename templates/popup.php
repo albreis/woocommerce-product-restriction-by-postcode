8109fa1477279d11314b911e81148a9d1ad5506b
Albreis - Design & Programação <albreisdev@gmail.com>
2021-10-14 20:45:27 -0300

    Suporte a traduções

--- templates/popup.php
+++ templates/popup.php
@@ -1,11 +1,11 @@
 <script>window.ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';</script>
-<div class="product-restriction-by-postcode" data-popup <?php if(isset($_SESSION['product_restriction_by_postcode']) && !empty($_SESSION['product_restriction_by_postcode'])): ?>style="display:none"<?php endif; ?>>
+<div class="restringir-produto-por-cep" data-popup <?php if(WC()->customer->get_billing_postcode()): ?>style="display:none"<?php endif; ?>>
   <div class="overlay"></div>
   <div class="box">
-    <strong>Onde você está?</strong>
-    <p>Dessa forma você terá acesso aos produtos e ofertas da sua região</p>
-    <span>Digite abaixo o seu CEP</span>
-    <input maxlength="9" inputmode="numeric" pattern="[\d]{8}-[\d]{3}" type="text" name="postcode" placeholder="Digite o CEP e pressione enter">
-    <button>Confirmar</button>
+    <strong><?php echo __('Onde você está?', 'restringir-produto-por-cep'); ?></strong>
+    <p><?php echo __('Dessa forma você terá acesso aos produtos e ofertas da sua região', 'restringir-produto-por-cep'); ?></p>
+    <span><?php echo __('Digite abaixo o seu CEP', 'restringir-produto-por-cep'); ?></span>
+    <input maxlength="9" inputmode="numeric" pattern="[\d]{8}-[\d]{3}" type="text" name="postcode" placeholder="<?php echo __('Digite o CEP e pressione enter', 'restringir-produto-por-cep'); ?>">
+    <button><?php echo __('Confirmar', 'restringir-produto-por-cep'); ?></button>
   </div>
 </div>
--- templates/restriction.php
+++ templates/restriction.php
@@ -1,6 +1,6 @@
 <?php if(defined('RESTRICTED')): ?>
-<div class="product-restriction-by-postcode-alert">
-  <strong>Atenção!</strong> Este produto não está disponível para entrega e por isso a calculadora de frete está desativada. <a href="#" id="popupcep">CLIQUE AQUI</a> para mudar o CEP.
+<div class="restringir-produto-por-cep-alert">
+<?php echo __('<strong>Atenção!</strong> Este produto não está disponível para entrega e por isso a calculadora de frete está desativada. <a href="#" id="popupcep">CLIQUE AQUI</a> para mudar o CEP.', 'restringir-produto-por-cep'); ?>
 </div>
 <style>#shipping-simulator{display:none}</style>
 <?php endif; ?>
